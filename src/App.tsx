783bf5df37c950f385830fbac80583fe417ec5c5
Umme Zaiba <83930541+Umme-Zaiba@users.noreply.github.com>
2024-06-28 19:49:01 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import PasswordGenerator from './components/PasswordGenerator';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Password generator: </h2>
+      <PasswordGenerator/>
+    </div>
+  );
+}
+
+export default App;
